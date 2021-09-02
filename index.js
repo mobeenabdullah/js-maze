8b21e39c99fdadc88864d457b2e059e3e25c8878
Mobeen Abdullah <mobeenabdullah@gmail.com>
2021-09-03 04:06:45 +0500

    Making boilerplate

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hi Maze!');
+const { Engine, Render, Runner, World, Bodies } = Matter;
+
+const engine = Engine.create();
+const { world } = engine;
+const render = Render.create({
+    element: document.body,
+    engine: engine,
+    options: {
+        width: 600,
+        height: 500
+    }
+});
+Render.run(render);
+Runner.run(Runner.create(), engine);
+
+const shape = Bodies.rectangle(200, 200, 75, 75, {
+    isStatic: true
+});
+
+World.add(world, shape);
