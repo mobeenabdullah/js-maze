78bace3641861e3f5c939950aec66a43c806cda6
Mobeen Abdullah <mobeenabdullah@gmail.com>
2021-09-03 13:49:43 +0500

    Updating horizontal wall values

--- index.js
+++ index.js
@@ -69,9 +69,9 @@ const stepThroughCell = (row, column) => {
 
     // Assemble randomly-ordered list of neighbors
     const neighbors = shuffle([
-        // [row - 1, column, 'up'],
-        // [row, column + 1, 'right'],
-        // [row + 1, column, 'down'],
+        [row - 1, column, 'up'],
+        [row, column + 1, 'right'],
+        [row + 1, column, 'down'],
         [row, column - 1, 'left']
     ]);
 
@@ -94,9 +94,13 @@ const stepThroughCell = (row, column) => {
             verticals[row][column - 1] = true;
         } else if(direction === 'right') {
             verticals[row][column] = true;
+        } else if(direction === 'up') {
+            horizontals[row - 1][column] = true;
+        } else if(direction === 'down') {
+            horizontals[row][column] = true;
         }
     }
 
     // Visit that next cell
 };
-stepThroughCell(1, 1);
+stepThroughCell(startRow, startColumn);
