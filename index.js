885f09fd947b5785cb6159eb8d2557411c261abd
Mobeen Abdullah <mobeenabdullah@gmail.com>
2021-09-03 11:52:20 +0500

    Verticals and Horizontals

--- index.js
+++ index.js
@@ -31,5 +31,13 @@ const grid = Array(3)
     .fill(null)
     .map(() => Array(3).fill(false));
 
-console.log(grid);
+const verticals = Array(3)
+    .fill(null)
+    .map(() => Array(2).fill(false));
+
+const horizontals = Array(2)
+    .fill(null)
+    .map(() => Array(3).fill(false));
+
+console.log(grid, verticals, horizontals);
 
