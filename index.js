cace00e28b903b026fc4f951ba4b36ffccec9a01
Mobeen Abdullah <mobeenabdullah@gmail.com>
2021-09-03 13:09:49 +0500

    Determining Movement Direction

--- index.js
+++ index.js
@@ -69,19 +69,29 @@ const stepThroughCell = (row, column) => {
 
     // Assemble randomly-ordered list of neighbors
     const neighbors = shuffle([
-        [row - 1, column],
-        [row, column + 1],
-        [row + 1, column],
-        [row, column - 1]
+        [row - 1, column, 'up'],
+        [row, column + 1, 'right'],
+        [row + 1, column, 'down'],
+        [row, column - 1, 'left']
     ]);
 
     // For each neighbor: ...
+    for(let neighbor of neighbors) {
+        const [nextRow, nextColumn, direction] = neighbor;
 
-    // See if that neibghors is out of bounds
+        // See if that neibghors is out of bounds
+        if(nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) {
+            continue;
+        }
 
-    // If we've visited that neibghor, continue to next neighbor
+        // If we've visited that neibghor, continue to next neighbor
+        if(grid[nextRow][nextColumn]) {
+            continue;
+        }
 
-    // Remove a wall from either horizontals or verticals
+        // Remove a wall from either horizontals or verticals
+        
+    }
 
     // Visit that next cell
 };
