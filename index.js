61a4f8f6ea27f790de93da8e2b907a5c65b409b0
Mobeen Abdullah <mobeenabdullah@gmail.com>
2021-09-04 02:45:37 +0500

    Adding fill colors and displaying a success message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cellsHorizontal = 5;
-const cellsVertical = 5;
+const cellsHorizontal = 14;
+const cellsVertical = 10;
 const width = window.innerWidth;
 const height = window.innerHeight;
 
@@ -15,7 +15,7 @@ const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        wireframes: true,
+        wireframes: false,
         width: width,
         height: height
     }
@@ -127,7 +127,10 @@ horizontals.forEach((row, rowIndex) =>{
             5,
             {
                 label: 'wall',
-                isStatic: true
+                isStatic: true,
+                render: {
+                    fillStyle: 'red'
+                }
             }
         );
         World.add(world, wall);
@@ -147,7 +150,10 @@ verticals.forEach((row, rowIndex) =>{
             unitLengthY,
             {
                 label: 'wall',
-                isStatic: true
+                isStatic: true,
+                render: {
+                    fillStyle: 'red'
+                }
             }
         );
         World.add(world, wall);
@@ -162,7 +168,10 @@ const goal = Bodies.rectangle(
     unitLengthX * 0.7,
     {
         label: 'goal',
-        isStatic: true
+        isStatic: true,
+        render: {
+            fillStyle: 'green'
+        }
     }
 );
 World.add(world, goal);
@@ -173,7 +182,12 @@ const ball = Bodies.circle(
     unitLengthX / 2,
     unitLengthY / 2,
     ballRadius,
-    { label: 'ball' }
+    {
+        label: 'ball',
+        render: {
+            fillStyle: 'blue'
+        }
+    }
 );
 World.add(world, ball);
 
@@ -206,6 +220,7 @@ Events.on(engine, 'collisionStart', event => {
             labels.includes(collision.bodyA.label) && 
             labels.includes(collision.bodyB.label)
         ) {
+            document.querySelector('.winner').classList.remove('hidden');
             world.gravity.y = 1;
             world.bodies.forEach(body => {
                 if (body.label === 'wall') {
